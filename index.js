2362c69f0cf15836da439c08a1b5de648ac6dcef
Tara Kelly <tara.kelly16@gmail.com>
2021-05-24 12:34:14 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+const nameArray = ["Tara", "Joe", "Claire"];
+const event = "birthday";
+
+function writeCards(nameArray, event) {
+    let messageArray = [];
+    for (let i=  0; i < nameArray.length; i++) {
+        messageArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return messageArray;
+}
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+      console.log(i--);
+    } 
+}
