6ff0090b1e58f506843071dbe9b61cb9778b64af
Tara Kelly <tara.kelly16@gmail.com>
2021-06-15 15:14:48 -0400

    fixed while loop

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts); 
 
 const nameArray = ["Tara", "Joe", "Claire"];
 const event = "birthday";
 
 function writeCards(nameArray, event) {
     let messageArray = [];
-    for (let i=  0; i < nameArray.length; i++) {
+    for (let i=0; i < nameArray.length; i++) {
         messageArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`);
-        debugger;
     }
     return messageArray;
 }
 
+console.log(writeCards(nameArray, event));
+
+
 function countDown(number) {
     let i = number;
     while (i >= 0) {
-      console.log(i--);
+    console.log(i)
+    i--;
     } 
 }
+countDown(10);
